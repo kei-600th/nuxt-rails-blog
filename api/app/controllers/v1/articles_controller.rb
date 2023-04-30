5befb271c6272945c666edf862b8304190df3070
kei-600th <kei19941005@gmail.com>
2023-04-30 11:09:43 +0900

    articlesコントローラの作成

---
+++ api/app/controllers/v1/articles_controller.rb
@@ -0,0 +1,32 @@
+class V1::ArticlesController < ApplicationController
+
+  def index
+    article = Article.all
+    render json: article
+  end
+
+  def create
+    article = Article.new(article_params)
+    if article.save
+      render json: article
+    else
+      render json: article.errors
+    end
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    if article.destroy
+      render json: article
+    end
+  end
+
+  private
+
+  # ストロングパラメーターの設定
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+
+end
